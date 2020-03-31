6d7a576ac42a27797e666f77bbaa7baebcb2f97b
Loïc Niederhauser <loic.niederhauser@gmail.com>
2020-03-31 22:42:25 +0200

    Added data statistics computation an representation

--- source/main.py
+++ source/main.py
@@ -3,4 +3,14 @@
 import preprocessing as pp
 import metadata as md
 
-trainingData = pp.DataContainer(md.trainingFileName, md.dataPath)
+if __name__ == '__main__':
+    trainingData = pp.DataContainer(md.trainingFileName, md.dataPath)
+    trainingData.plotStats()
+    dataStat = trainingData.get_dataStat()
+    print("\n=========== DATA STATISTICS ===========\n"
+          "Average Open ports:   ", dataStat['mean'], "\n"
+          "Max Open Ports:       ", dataStat['max'], "\n"
+          "Min Open Ports:       ", dataStat['min'], "\n"
+          "Total time:           ", dataStat['totalTime'], "[s]\n"
+          "Number of data points:", dataStat['nbData'], "\n"
+          "=======================================\n")
--- source/preprocessing.py
+++ source/preprocessing.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from os import path
 import gc
+import matplotlib.pyplot as plt
+
 
 class DataContainer:
 
@@ -8,6 +10,13 @@ class DataContainer:
     _df = []
     _fileName = ''
     _dataIsLoaded = False
+    _dataStat = {'mean': float('nan'),
+                 'histogram': {},
+                 'nbData': float('nan'),
+                 'totalTime': float('nan'),
+                 'min': float('nan'),
+                 'max': float('nan')}
+    _dataStatIsLoaded = False
 
     def __init__(self, fileName, filePath=''):
         self.loadData(fileName, filePath)
@@ -39,6 +48,28 @@ class DataContainer:
             self._dataIsLoaded = False
             gc.collect()  # Force garbage collection. Safer for big df.
 
+        # clear dataStats
+        _dataStat = {'mean': float('nan'),
+                     'histogram': {},
+                     'nbData': float('nan'),
+                     'totalTime': float('nan'),
+                     'min': float('nan'),
+                     'max': float('nan')}
+        self._dataStatIsLoaded = False
+
+    def plotStats(self):
+        if not self._dataStatIsLoaded:
+            self._computeDataStat()
+        x = list(self._dataStat['histogram'].keys())
+        y = [0] * (max(x)+1)
+        for index, value in enumerate(x):
+            y[index] = self._dataStat['histogram'][value]
+
+        plt.bar(x, y)
+        plt.xlabel('Number of open channels')
+        plt.ylabel('Number of occurrences')
+        plt.show()
+
     # region getters
     def get_expectedExtension(self):
         return self._expectedExtension
@@ -51,4 +82,45 @@ class DataContainer:
 
     def get_dataIsLoaded(self):
         return self._dataIsLoaded
+
+    def get_dataStat(self):
+        if not self._dataIsLoaded:
+            Exception('Data should be loaded before computing stats')
+        else:
+            self._computeDataStat()
+        return self._dataStat
+
+    def get_dataStatIsLoaded(self):
+        return self._dataStatIsLoaded
+
+    # endregion
+
+    # region private functions
+    def _computeDataStat(self):
+        if not self._dataStatIsLoaded:
+            self._dataStat['mean'] = self._computeSignalMean(self._df)
+            self._dataStat['histogram'] = self._computeHistogram(self._df)
+            self._dataStat['nbData'] = self._computeNbRow(self._df)
+            self._dataStat['totalTime'] = self._computeTotalTime(self._df)
+            self._dataStat['min'] = min(list(self._dataStat['histogram'].keys()))
+            self._dataStat['max'] = max(list(self._dataStat['histogram'].keys()))
+
+    # endregion
+
+    # region static methods
+    @staticmethod
+    def _computeSignalMean(df):
+        return df.signal.mean()
+
+    @staticmethod
+    def _computeHistogram(df):
+        return df.open_channels.value_counts().to_dict()
+
+    @staticmethod
+    def _computeNbRow(df):
+        return len(df.index)
+
+    @staticmethod
+    def _computeTotalTime(df):
+        return max(df.time)
     # endregion
